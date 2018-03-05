da2c0c0e00088aa35f65c3e1c89e1b1b48c256e7
rosemarycadams.gmail.com <rosemarycadams@gmail.com>
2018-03-04 20:03:28 -0800

    regressions

--- project/regressions.R
+++ project/regressions.R
@@ -2,46 +2,43 @@
 ###################### Log Regressions ###############################
 ######################################################################
 
+library(dplyr)
+
 #read in data, get rid of state and country data
-data <-read.csv("./data/clean_data.csv", stringsAsFactors = FALSE) %>% select(-state, -Country, -X, -work_interfere)
+data <-read.csv("./data/clean_data.csv", stringsAsFactors = FALSE) %>% select(-state, -Country, -X, -work_interfere, -mental_health_consequence, -phys_health_consequence)
 
 healthy_data <- read.csv("./data/healthy_data.csv", stringsAsFactors = FALSE) %>% select(-state, -Country, -X)
 illness_data <- read.csv("./data/illness_data.csv", stringsAsFactors = FALSE) %>% select(-state, -Country, -X)
 
-##predict and run log regressions
-#predIll = glm.predict()
-
 ##running the log regression on all covariates
-#log_regression <- function(covariate){
-#  glm(social_acceptance ~ data[[covariate]], family=binomial(link='logit'),data=data)
-#  return (glm_social)
-#}
-
-##running the log regression on all covariates & new variables
-log_regression_com <- function(covariate, col) {
-  glm(col ~ data[[covariate]], family = binomial(link='logit'), data=data)
-  return (glm)
+log_regression <- function(covariate){
+  l <- glm(social_acceptance ~ data[[covariate]], family=binomial(link='logit'), data=data, maxit=100)
+  print(summary(l))
+  return (l)
 }
 
-#predRed = glm.predict()
-#data['def_pred_binary'] = (data['df_predict'] > 0.5)
-#df['def_pred_binary'] = df['def_pred_binary'].apply(lambda x: 1 if x == True else 0)
-#plt.show()
-
-#data['df_predict'] = inter_model.predict()
+##running the log regression on all covariates & new variables
+#log_regression_com <- function(covariate, col) {
+#  glm(col ~ data[[covariate]], family = binomial(link='logit'), data=data)
+#  return (glm)
+#}
 
 for(covariate in colnames(data)){
   log_regression(covariate)
 }
 
+summary(glm(social_acceptance ~., family=binomial(link='logit'), data=data), maxit=100)
+
+
+summary(glm(social_acceptance ~ Gender+self_employed, family=binomial(link='logit'), data=data, maxit=100))
+
+
+
+
+
+
+
+
 glm_social <- glm(social_acceptance ~ mental_health_consequence, family=binomial(link='logit'),data=data)
 data['predict'] = glm_social.predict()
 glm_social.summary()
-
-
-data <-read.csv("./data/clean_data.csv", stringsAsFactors = FALSE) %>% select(-state, -Country, -X, -work_interfere, -mental_health_consequence, -phys_health_consequence)
-
-data <- data %>% select(Gender, self_employed, family_history, social_acceptance, treatment, no_employees, remote_work, tech_company, 
-                                benefits, care_options, wellness_program, seek_help, anonymity, leave, coworkers, supervisor)
-
-glm_social <- glm(social_acceptance ~ ., family=binomial(link='logit'),data=data)
