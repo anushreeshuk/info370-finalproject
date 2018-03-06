fac5372806d42ed3344993cae97ea73047660596
Rosemary <rosemarycadams@gmail.com>
2018-03-06 08:43:38 -0800

    scaffolding UI

--- project/ui.R
+++ project/ui.R
@@ -10,24 +10,39 @@
 library(shiny)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
-  )
-))
+shinyUI(navbarPage("Mental Health in the Workplace",
+            tabPanel(
+              "Overview",
+              mainPanel(
+                "This is where we talk about the data"
+              )
+            ),
+            tabPanel("Explore",
+              sidebarLayout(
+                sidebarPanel(
+                  checkboxGroupInput("selectData", label = h3("Select your data of interest"), 
+                                     choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
+                                     selected = 1)
+                  ,
+                  checkboxGroupInput("selectOutcome", label = h3("Select your outcome of interest"), 
+                                   choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
+                                   selected = 1)
+                  ,
+                  checkboxGroupInput("selectCovariates", label = h3("Select your covariates of interest"), 
+                                    choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
+                                    selected = 1)
+                  ),
+                       
+                  mainPanel(
+                    fluidRow(column(3, verbatimTextOutput("data"))),
+                    fluidRow(column(3, verbatimTextOutput("outcome"))),
+                    fluidRow(column(3, verbatimTextOutput("covariate")))
+                    
+                  )
+            )),
+            tabPanel("Conclusions",
+              mainPanel(
+                "This is where we draw our conclusions"
+              )
+            ), inverse=TRUE
+       ))
