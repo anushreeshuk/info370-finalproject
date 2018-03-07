c37bf27a68090f482b99a2fbecce9a151a049f30
anushreeshuk <anushreeshuk@gmail.com>
2018-03-07 00:27:53 -0800

    fixed errors

--- project/bestfit.R
+++ project/bestfit.R
@@ -1,15 +1,7 @@
 
-library(ROCR)
 library(ggplot2)
 
-# distributions
-# compare how much more it
-
-#turn it into a fucntion that will take in a coovariate vector of coovariate
-# outcome of interest
-# data
-
-###function that takes in a dataset, covariates and metric to evaluate and spits out the glm_model
+#function that takes in a dataset, covariates and metric to evaluate and spits out the glm_model
 create_glm_model <- function(covariates, data, metric) {
   
   #starts off with the first covariate the user picks
@@ -21,47 +13,35 @@ create_glm_model <- function(covariates, data, metric) {
       equation <- paste(equation, covariate, sep = " + ")
     }
   }
-
-  #create a glm_model by passing in the equation with covariates, data, metric.
+  
+  #create a glm_model by passing in the equation with covariates, data, metric
   glm_model <- glm(as.formula(equation), family=binomial(link='logit'), data=data, maxit=100)
+  return(glm_model)
+}
+
+#store this value
+glm_model_val <- create_glm_model(covariates, data, metric)
+
+#calculate and draw best fit curve for sensitivity and false positive for given glm logistic model
+draw_best_fit <- function(glm_model_val, covariates, data, metric) {
+  fitmetric_pos <- glm_model_val$fitted.values[data[metric] == 1]
+  fitmetric_neg <- glm_model_val$fitted.values[data[metric] == 0]
+  sort_fitmetric <- sort(glm_model_val$fitted.values)
   
-  #set and sort fitted positive and negative metrics to binary values
-  fitmetric_pos <- glm_model$fitted[data$metric == 1]
-  fitmetric_neg <- glm_model$fitted[data$metric == 0]
-  sort_fitmetric <- sort(glm_model$fitted.values)
-  
-  #set initial values to zero
-  sensitivity <- 0
-  specificity <- 0
-  
-  #calculate and set the sensitivity and specificity
+  sens <- 0
+  spec <- 0
+
   for (i in length(sort_fitmetric):1) {
-    sensitivity <- c(sensitivity, mean(fitmetric_pos >= sort_fitmetric[i]))
-    specificity <- c(specificity, mean(fitmetric_neg >= sort_fitmetric[i]))
-  }
-  #calculate and predict sensitivity and false positive measure for glm logistic model
-  fitmetric_pos_pred <- as.numeric(glm_model$pred[data$metric == 1]) - 1
-  fitmetric_neg_pred <- as.numeric(glm_model$pred[data$metric == 0]) - 1
-  sort_fitmetric_pred <- as.numeric(sort(glm_model$pred)) - 1
- 
-  sensitivity_pred <- 0
-  specificity_pred <- 0
-  
-  for (i in length(sort_fitmetric_pred):1) {
-    sensitivity_pred <- (c(sensitivity_pred, mean(fitmetric_pos_pred >= sort_fitmetric_pred[i])))
-    specificity_pred <- (c(specificity_pred, mean(fitmetric_neg_pred >= sort_fitmetric_pred[i])))
+    sens <- c(sens, mean(fitmetric_pos >= sort_fitmetric[i]))
+    spec <- c(spec, mean(fitmetric_neg >= sort_fitmetric[i]))
   }
   
-  #plot the best fit line
-  best_fit_plot <- plot(specificity, sensitivity, xlim = c(0, 1), ylim = c(0, 1), type = "l", xlab = "False Positive Rate", ylab = "True Positive Rate", col = 'blue')
- 
-   #create costant abline
+  # draw plot
+  best_fit_plot <- plot(spec, sens, xlim = c(0, 1), ylim = c(0, 1), type = "l", xlab = "False Positive Rate", ylab = "True Positive Rate", col = 'blue')
   abline(0, 1, col = "black")
-  lines(specificity_pred, sensitivity_pred, col='green')
   legend("topleft", legend = c("logit") , pch = 15, bty = 'n', col = c("blue"))
   return(best_fit_plot)
 }
-
 
 ############### TESTING ###############
 
@@ -70,17 +50,8 @@ healthy_data <- read.csv("./data/healthy_data.csv", stringsAsFactors = FALSE) %>
 illness_data <- read.csv("./data/illness_data.csv", stringsAsFactors = FALSE) %>% select(-state, -Country, -X)
 
 # Function test dummy data
-covariates <- c("supervisor", "leave", "care_options", "wellness_program", "mental_health_interview")
-metric <- "social_acceptance"
+covariates <- c("leave", "care_options", "wellness_program", "mental_health_interview")
+metric <- "ease_communication"
 
-# Get the dataframe, then create the histogram
-create_glm_model(covariates, data, metric)
-
-
-
-# I FIRST NEEDED TO DO THIS THEN REALISED I DONT HAVE TO
-# Keeping this here incase I do
-# splitting the data into training and testing
-# train<-sample_frac(data, 0.75)
-# sid <- as.numeric(rownames(train)) # because rownames() returns character
-# test <- data [-sid,]
+####### YAY IT WORKS #########
+draw_best_fit(glm_model_val, covariates, data, metric)
